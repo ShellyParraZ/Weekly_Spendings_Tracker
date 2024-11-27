d93fc253026eeaf22a6ed4760de78138fc2df737
ShellyParraZ <shellyxparra@gmail.com>
2024-11-26 22:12:27 -0500

    Update spendings_tracker.py
    Completely changed every aspect of the previous submission. I kept the concept of a Week class. The __repr__ method is still used. I added various methods and functions. I added a main function to better handle the program.

--- spendings_tracker.py
+++ spendings_tracker.py
@@ -1,53 +1,51 @@
-""" This program will track your weekly spendings. It will automatically submit a summary by the
-beginning of each Sunday. The apps target audience are college students.
+""" This program will provide an in-depth analysis on your weekly spendings.
+The apps target audience are college students.
 
 Driver/Navigator: Shelly Parra
 Assignment: Check-in 2
-Date: 10/27/2024
+Date: 10/26/2024
 
-Challenges Encountered: 
+Challenges Encountered: Understanding databases.
 
 """
 
-# whenever a user inputs a description 
-# that is similar to a previous submission, then it will add to the previous submission.
-# For example, I add "coffee, $5.00", then later "CoFFee, $2,50". The dictionary will update the previous
-# submission to "coffee, $7.50". All the dictionary descriptions will be lowercase.
 
-
-import datetime
-import sys
-import argparse
 import csv
-# allow the user to input the date they want the report?
+import sqlite3
+import os
+
 
 class Week:
-    """ A weekly tracker of the users spendings. It will provide a report of the previous week on Sundays.
+    """ A weekly tracker of the users spendings. 
     
     Attributes:
-        daily_spendings (dict): a dictionary containing the daily spendings of the user.
-        self.date (date): The date of the sunday of the week.
+        database_conn (connection): The connection to the database.
         self.weekly_tuition_perc (float): The weekly percentage of money spent on tuition.
         self.weekly_rent_perc (float): The weekly percentage of money spent on rent.
         self.weekly_utilities_perc (float): The weekly percentage of money spent on utilities.
         self.starting_amount (float): The amount of money the user has at the beginning of the week.
         self.want_spendings_perc (float): The percentage of spendings on total wants.
         self.need_spendings_perc (float): The percentage of spendings on total needs.
+        self.total_spendings (float): The total amount of spendings, specifically the wants and needs.
+        self.total_want_spendings (float): The total amount of spendings on wants.
+        self.total_need_spendings (float): The total amount of spendings on needs.
+        self.windfall (float): The amount of money the user unexpectedly gathered.
+        self.overdraft (float): The amount of money the user used past their spendings amount and windfall.
+        self.remaining_balance (float): The amount of money the user has left after substracting from starting amount.
     
     """
     
     
-    def __init__(self, daily_spendings, yearly_salary = 0.00, tuition = 0.00, rent = 0.00, utilities = 0.00):
+    def __init__(self, database_conn):
         
-        self.daily_spendings = []
-        self.daily_spendings.append(daily_spendings) # a full week, each weekday is a tuple with two dictionaries
-        self.date = datetime.date.today() # the current date
+        self.database_conn = database_conn
+        self.cursor = database_conn.cursor() # used to create queries
         
-        # weekly versions of the yearly_salary, tuition, rent, and utilities
-        self.weekly_salary = (yearly_salary / 12) / 4
-        self.tuition = (tuition / 12) / 4
-        self.rent = (rent / 12) / 4
-        self.utilities = (utilities / 12) / 4
+        # create table query
+        cq = f'''CREATE TABLE week (
+                id INTEGER PRIMARY KEY, priority_type TEXT, category TEXT, name TEXT, cost FLOAT
+                )'''
+        self.cursor.execute(cq) # creates table
         
         # percentage version
         self.weekly_tuition_perc = (self.tuition / self.weekly_salary) * 100
@@ -55,157 +53,243 @@ class Week:
         self.weekly_utilities_perc = (self.utilities / self.weekly_salary) * 100
         
         self.starting_amount = self.weekly_salary - (self.tuition + self.rent + self.utilities)
-        
-        # percentage of wants and needs
-        self.want_spendings_perc = (self.total_want_spendings / self.total_spendings) * 100
-        self.need_spendings_perc = (self.total_need_spendings / self.total_spendings) * 100
-        
-        # might have to do this in if name == "main"
-        if datetime.date(self.date).weekday() == 6 and len(self.daily_spendings) == 7: # sunday and all seven days are included
-            # call a method that will add all the information within the dictionaries
-            thorough_analysis(self.daily_spendings)
             
             
-    def thorough_analysis(self):
-        """ Combines the information in the dictionaries in order to provide thorough analysis of
-        wants and needs. In otherwords, combines duplicates.
+    def insert_data(self, file_path):
+        """ Inserts the data from the file to the table.
         
-        Returns:
-            self.remaining_balance (float): The amount of money that remains after the spendings are subtracted.
-            self.overdraft (float): The amount of money the user owes. Gets updated if the remaining balance is negative.
+        Attributes:
+            file_path (str): The file path.
+        
+        Special Effects:
+            Each row in the file is implemented into the week table.
         
         """
         
-        # create a dictionary that will have all the combined information
-        dict_summary = {}
+        # should I consider rows that have missing columns!!!!!!
         
-        # delete all the empty strings that represent the empty days
-        updated_daily_spend = [x for x in self.daily_spendings if x != "empty"]
+        with open(file_path, 'r', encoding = 'utf-8') as csv_file: # to read the file
+            csv_reader = csv.reader(csv_file, delimiter = ',') # object, the comma is default
+            
+            # convert to list
+            file_list = list(csv_reader)
+            
+            data = [] # empty list that will get populated by the data inside the csv
+            
+            for row in file_list: # adds all the data to the list
+                data.append(row)
+                
+            imq = f'''INSERT INTO week VALUES(?,?,?,?)'''
+            self.cursor.executemany(imq, data) # inserts the data      
+            
+            self.database_conn.commit() # commits changes 
+            
+    
+    def retrieve_analysis(self, weekly_info):
+        """ Based on all the data gathered, an analysis is provided.
         
-        # Combine the value of all the keys that contain the same name
-        for day in updated_daily_spend: # day = tuple
-            for dict in day: # for each dictionary in the tuple
-                for key in dict: # for each key in dictionary
-                    if dict_summary.get(key, False) == False: # if False, then key does not exist
-                        dict_summary[key] = updated_daily_spend[key]
-                    else: # the key already exists, so add the previous value to the current value
-                        dict_summary[key] = dict_summary[key] + updated_daily_spend[key]
+        Attributes:
+            weekly_info (dict): Containing the weekly salary, tuition, rent, and utilities.
+            
+        Special Effects:
+            Using the week table, different variations of information is collected an assigned a variable.
         
-        # self.remaining_balance: needs to be determined after adding the 
+        """
+        
+        # the amount of money that is left after considering the unavoidable financial payments
+        starting_amount = weekly_info["Salary"] - (weekly_info["Tuition"] + weekly_info["Rent"] + weekly_info["Utilities"])
+        
+        # locate the "Windfall" in priority type column, then add the costs in those rows
+        sq1 = '''SELECT SUM(cost) FROM week WHERE priority_type = "Windfall"'''
+        self.cursor.execute(sq1)
+        self.windfall = self.cursor.fetchall() # the sum of the windfall
+        
+        # calculate the total spendings by finding the sums of all the rows, except windfall
+        sq2 = '''SELECT SUM(cost) FROM week WHERE priority_type != "Windfall"'''
+        self.cursor.execute(sq2)
+        self.total_spendings = self.cursor.fetchall()
+        
+        # locate the "Want" in priority type column, then add the costs in those rows
+        sq3 = f'''SELECT SUM(cost) FROM week WHERE priority_type = "Want"'''
+        self.cursor.execute(sq3)
+        self.spending_wants = self.cursor.fetchall()
+        
+        # get the want percentage
+        self.want_spendings_perc = (self.spending_wants / self.total_spendings) * 100
+        
+        # locate the "Need" in priority type column, then add the costs in those rows
+        sq4 = f'''SELECT SUM(cost) FROM week WHERE priority_type = "Need"'''
+        self.cursor.execute(sq4)
+        self.spending_needs = self.cursor.fetchall()
+        
+        # get the need percentage
+        self.need_spendings_perc = (self.spending_needs / self.total_spendings) * 100
+        
+        # get the overdraft and remaining balance
+        overdraft_balance = (starting_amount + self.windfall) - self.total_spendings
+        if overdraft_balance < 0.0:
+            self.overdraft = overdraft_balance * -1.0
+            self.remaining_balance = 0.0
+        else:
+            self.overdraft = 0.0
+            self.remaining_balance = overdraft_balance
+        
+        
+    def category_analysis(self, category):
+        """ Calculates the percentage of each category.
+        
+        Special Effects:
+            Prints the categories and their percentages.
+        
+        """
         
         
     def __repr__(self):
         """ Prints out all the information gathered regarding the week.
         
+        Side Effects:
+            All the data is structured and organized to get printed.
+        
         """
         
         print("Weekly Summary")
         print()
-        print(f"Date: {self.date}")
+        print(f"Starting Amount: ${self.starting_amount}")
+        print(f"Remaining Balance: ${self.remaining_balance}")
+        print(f"Overdraft: ${self.overdraft}")
+        print(f"Windfall: {self.windfall}")
+        print(f"Weekly Tuition Percentage: {self.weekly_tuition_perc}%")
+        print(f"Weekly Rent Percentage: {self.weekly_rent_perc}%")
+        print(f"Weekly Utilities Percentage: {self.weekly_utilities_perc}%")
+        print(f"Total Spendings: ${self.total_spendings}")
         print()
-        
-        pass
-
-
-class Day: # what relationship would this have with the other class, maybe composition?
-    """ Every day in a week. It will collect the information for each individual day.
-    
-    Attributes:
-        self.total_spendings (float): The total amount of spendings, specifically the wants and needs.
-        self.total_want_spendings (float): The total amount of spendings on wants.
-        self.total_need_spendings (float): The total amount of spendings on needs.
-        self.windfall (float): The amount of money the user unexpectedly gathered.
-    
-    """
-    
-    # We need to collect the file and then get all the information.
-    
-    def __init__(self, journal_file_path):
-        
-        self.journal_file_path = journal_file_path
-        self.total_spendings = 0.00
-        self.total_want_spendings = 0.00
-        self.total_need_spendings = 0.00
-        self.windfall = 0.00
-            
-    
-    def parsed_file(self):
-        """ Creates two dictionaries for each day of the week.
-        
-        Attributes:
-            self.windfall (float): The amount of money the user unexpectedly gathered.
-        
-        Returns:
-            A tuple with two dictionaries. The first element in the tuple is a dictionary of the spending wants and
-            the second element in the tuple is a dictionary of the spending needs.
-        
-        """
-        # a database would store their information overtime!!!
-        # allows you to import their information as a table
-        # like columns  (want, need, windfall)
-        # to access later, you'd query the database
-        # DATABASES!!!
-        
-        # open the csv file
-        with open(self.journal_file_path, 'r', encoding = 'utf-8') as csv_file: # to read file
-            file_reader = csv.reader(csv_file)
-            
-            # convert to list
-            file_list = list(file_reader)
-            
-            # check if list is empty
-            if not file_list: # list is empty
-                return "empty"
-            
-            # the dictionary that will be returned
-            weekday_dict_wants = {}
-            weekday_dict_needs = {}
-            
-            for line in file_list:
-                if line[0].lower() == "want":
-                    weekday_dict_wants[line[1].lower()] = float(line[2])
-                    self.total_want_spendings += float(line[2])
-                elif line[0].lower() == "need":
-                    weekday_dict_needs[line[1].lower()] = float(line[2])
-                    self.total_need_spendings += float(line[2])
-                else:
-                    self.windfall += float(line[2])
-                    
-                self.total_spendings += float(line[2])
-            
-        return weekday_dict_wants, weekday_dict_needs
+        print(f"Spending Wants:")
+        print(f"\tTotal: ${self.spending_wants}")
+        print(f"\tSpending Percentage: {self.want_spendings_perc}%")
+        print()
+        print(f"Spending Needs:")
+        print(f"\tTotal: ${self.spending_needs}")
+        print(f"\tSpending Percentage: {self.need_spendings_perc}%")
+        print()
     
 
-def parse_args(args_list):
-    """ Allows user to input their csv file, yearly salary, tuition, rent, and utilities.
+def weekly_calculation(yearly_values):
+    """ Calculate the weekly salary, tuition, rent, and utilities based on the users yearly submission.
     
-    Attributes:
-        args_list (list[str]): A list of strings containing the command-line arguments for the program.
+    Attribute:
+        yearly_values (tuple): the yearly salary, tuition, rent, utilities.
     
     Returns:
-        The parsed arguments object.
+        general_info (dict): A dictionary of the weekly salary, tuition, rent and utilities.
+    
+    """
+    general_info = {}
+    
+    for index, item in enumerate(yearly_values):
         
+        updated_item = (item / 12) / 4
+        
+        if index == 0:
+            general_info["Salary"] = updated_item
+        elif index == 1:
+            general_info["Tuition"] = updated_item
+        elif index == 2:
+            general_info["Rent"] = updated_item
+        else:
+            general_info["Utilities"] = updated_item
+    
+    return general_info
+
+
+def general_info():
+    """ Collecting general information regarding the user.
+    
+    Returns:
+        A tuple containing the general information.
+    
     """
     
-    # initialize the parser
-    parser = argparse.ArgumentParser(description = 'Provide your csv file, yearly salary, tuition, rent, and utilities. Do not use the dollar sign.')
+    while True: 
+        yearly_salary = retrieve_float("Input your yearly salary: ")
+        yearly_tuition = retrieve_float("Input your yearly tuition: ")
+        yearly_rent = retrieve_float("Input your yearly rent: ")
+        yearly_utilities = retrieve_float("Input your yearly utilities: ")
+        
+        print("These are your current responses:")
+        print(f"Yearly Salary: {yearly_salary}")
+        print(f"Tuition: {yearly_tuition}")
+        print(f"Rent: {yearly_rent}")
+        print(f"Utilities: {yearly_utilities}")
+        restart = input("Would you like to change a response? (\"yes\" or \"no\") ")
+        if restart == "no":
+            return yearly_salary, yearly_tuition, yearly_rent, yearly_utilities
+
+
+def retrieve_float(prompt):
+    """ Insures the user is providing a float.
     
-    # add the arguments
-    parser.add_argument('csvFile', type = str, help = 'input your csv file')
-    parser.add_argument('yearlySalary', type = float, help = 'input your yearly salary')
-    parser.add_argument('tuition', type = float, help = 'input your yearly tuition')
-    parser.add_argument('rent', type = float, help = 'input your yearly rent')
-    parser.add_argument('utilities', type = float, help = 'input your yearly utilities')
+    Returns:
+        response (float): The response to the prompt.
     
-    return parser.parse_args(args_list)
+    """
+    
+    while True:
+        try:
+            response = float(input(prompt + "(otherwise, input 0.0) "))
+            return response
+        except ValueError: # not a float
+            print("Incorrect value. Please enter a float.")
+
+
+def main():
+    """ Prompts the user for general information. Handles the overall program.
+    
+    Attributes:
+        week (obj): Object of Week class.
+    
+    """
+    
+    print("Hello!")
+    print("This is the Weekly Spendings Tracker.")
+    print("In order to provide a significant analysis, please provide your")
+    print("yearly salary, tuition, rent, and utilities.")
+    info = general_info() # a tuple
+    print("Your information was successfully saved.")
+    
+    # calculate the weekly amount of the users general information
+    weekly_info = weekly_calculation(info) # a dictionary
+    
+    # create a database to collect the information in the csv files
+    conn = sqlite3.connect('week.db')
+    
+    # create a class that will collect all the information
+    week = Week(conn)
+    
+    # go through the files in your folder
+    folder_path = input("Provide the path to your weekly spendings folder: ")
+    
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            # check if the file is empty
+            file_size = os.stat(file).st_size
+            
+            if file_size > 0:
+                week.insert_data(file)
+                
+    week.retrieve_analysis(weekly_info)
+    
+    # print the basic information by using repr
+    week.__repr__()
+        
+    # thorough category analysis
+    
+        
+    # thorough name analysis
+    
+    
+    # After collecting and printing all the information, close the database connection
+    conn.close()
 
 
 if __name__ == "__main__":
-    
-    week_obj = Week()
-    args = parse_args(sys.argv[1:]) # contains two arguments
-    
-    day = Day(args.csvFile)
-    want_need_tuple = day.parsed_file()
-    
-    week_obj(want_need_tuple, args.yearlySalary, args.tuition, args.rent, args.utilities)
+    main()
